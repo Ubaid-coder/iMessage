455574ea9a631c182eb8c257bc6ec70c6b213718
unknown <mu0826960@gmail.com>
2026-06-26 03:15:07 -0700

    Set up Dockerfile for multi-stage build and add .dockerignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
-import 'dotenv/config';
 import cors from 'cors';
-import User from './models/user.model.js';
-import { connectDB } from './lib/db.js';
+
+import 'dotenv/config';
+
+import fs from 'node:fs';
+import path from 'node:path';
+
 import { clerkMiddleware } from '@clerk/express';
 
+import User from './models/user.model.js';
+import { connectDB } from './lib/db.js';
 
 
 const app = express();
+
 const port = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
+
+const publicDir = path.join(process.cwd(), "public");
 
 app.use(cors({ origin: FRONTEND_URL, credentials: true }));
 app.use(express.json());
@@ -20,6 +28,15 @@ app.get("/health", (req, res) => {
 
     res.status(200).json({ ok: true });
 })
+
+// if the public directory exists, serve the static file
+if(fs.existsSync(publicDir)){
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}",(req,res,next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+    });
+}
 
 app.listen(port, () => {
     connectDB();
